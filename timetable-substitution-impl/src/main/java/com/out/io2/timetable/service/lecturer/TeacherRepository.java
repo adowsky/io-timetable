94cb81f10f9d763e90f1d40b9f2767c9a195c966
Adrian Kosinski <segodj112@gmail.com>
2017-04-13 00:09:39 +0200

    conflicts fix

--- timetable-substitution-impl/src/main/java/com/out/io2/timetable/service/lecturer/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.out.io2.timetable.service.lecturer;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface TeacherRepository extends CrudRepository<TeacherDAO, Long> {
-}
--- timetable-substitution-impl/src/main/java/com/out/io2/timetable/service/model/Lecturer.java
+++ timetable-substitution-impl/src/main/java/com/out/io2/timetable/service/model/Lecturer.java
@@ -1,20 +1,20 @@
 package com.out.io2.timetable.service.model;
 
 
-public class Teacher {
+public class Lecturer {
     private final Long id;
     private final String name;
     private final String surname;
     private final String academicTitle;
 
-    public Teacher(Long id, String name, String surname, String academicTitle) {
+    public Lecturer(Long id, String name, String surname, String academicTitle) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.academicTitle = academicTitle;
     }
 
-    public Teacher(Long id) {
+    public Lecturer(Long id) {
         this(id, null, null, null);
     }
 
