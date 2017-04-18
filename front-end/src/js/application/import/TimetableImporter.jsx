eab2a822ab70ed6576125a0a6a30bed32d647043
Adrian Kosinski <segodj112@gmail.com>
2017-04-18 21:28:00 +0200

    parametrized file upload

--- front-end/src/js/application/import/TimetableImporter.jsx
+++ front-end/src/js/application/import/TimetableImporter.jsx
@@ -11,23 +11,46 @@ export default class TimetableImporter extends React.Component {
 
         this.state = {
             status: "",
-            timetable: null
+            timetable: null,
+            departments: [],
+            faculties: [],
+            form: {
+                department: "",
+                faculty: "",
+                semester: "",
+                group: ""
+            }
         };
 
         this.handle = this.handle.bind(this);
         this.send = this.send.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        this.context.restClient.getRequest("/api/departments")
+            .then(departments => this.setState({departments}));
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.form.department !== prevState.form.department) {
+            this.context.restClient.getRequest(`/api/departments/${this.state.form.department}`)
+                .then(faculties => this.setState(faculties));
+        }
     }
 
     send(event) {
         event.preventDefault();
-
+        const {department, faculty, semester, group} = this.state.form;
         const file = event.target.elements["timetable"].files[0];
         const fileReader = new FileReader();
         fileReader.onload = () => {
             const headers = {
                 "Content-Type": "text/csv"
             };
-            this.context.restClient.multipartPostRequest("http://localhost:8080/api/timetable", fileReader.result, headers)
+            this.context.restClient.multipartPostRequest(
+                `http://localhost:8080/api/departments/${department}/${faculty}/${semester}/${group}/timetable`,
+                    fileReader.result, headers)
                 .then(() =>this.setState({status: "COMPLETE"}))
                 .catch(() => this.setState({status: "FAIL"}));
         };
@@ -45,6 +68,19 @@ export default class TimetableImporter extends React.Component {
         this.setState(newState);
     }
 
+    handleChange(event) {
+        const {name, value} = event.target;
+        const newForm = Object.assign({}, this.state.form, {
+            [name] : value
+        });
+        const newState = Object.assign({}, this.state, {
+            form: newForm
+        });
+
+        this.setState(newState);
+
+    }
+
 
     render() {
         return (
@@ -53,6 +89,10 @@ export default class TimetableImporter extends React.Component {
                 handle={this.handle}
                 send={this.send}
                 status={this.state.status}
+                form={this.state.form}
+                departments={this.state.departments}
+                faculties={this.state.faculties}
+                handleChange={this.handleChange}
             />
         );
     }
--- front-end/src/js/application/import/TimetableImporterView.jsx
+++ front-end/src/js/application/import/TimetableImporterView.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import FacultyView from './FacultyView';
+
 const getProcessingViewForStatus = (status) => {
     switch (status) {
         case "SENDING":
@@ -28,20 +30,36 @@ export default class TimetableImporterView extends React.Component {
     render() {
         const filename = this.props.filename || "";
         const placeholderTransitionStyle = (this.props.filename) ? "filled" : "";
-        const shouldDisableButton = !this.props.filename || this.props.status === "SENDING";
-
+        const shouldDisableButton = (!this.props.filename || !this.props.form.faculty || !this.props.form.department || !this.props.form.semester || !this.props.form.group)
+            || this.props.status === "SENDING";
+        const faculties = this.props.faculties || [];
+        console.log(this.props.form.department);
         return (
             <div>
                 <form onSubmit={this.props.send}>
-                    <div className="file-chooser">
-                        <span >
+                    <div className="selectable">
+                        <label>Department</label>
+                        <select value={this.props.form.department} name="department" onChange={this.props.handleChange}>
+                            <option />
+                            {this.props.departments.map(d => <option value={d} key={d}>{d}</option>)}
+                        </select>
+
+
+                    <FacultyView form={this.props.form} faculties={faculties} handleChange={this.props.handleChange}/>
+                    </div>
+                    <div>
+                        <div className="file-chooser">
+                        <span>
                             <h4 className={`input-placeholder ${placeholderTransitionStyle}`}>Plik z planem</h4>
                             <h4>{filename}</h4>
                         </span>
-                        <label htmlFor="timetable">Wybierz plik</label>
-                        <input type="file" name="timetable" id="timetable" accept=".csv" onChange={this.props.handle}/>
+                            <label htmlFor="timetable">Wybierz plik</label>
+                            <input type="file" name="timetable" id="timetable" accept=".csv"
+                                   onChange={this.props.handle}/>
+                        </div>
+                        <input type="submit" disabled={shouldDisableButton}/>
                     </div>
-                    <input type="submit" disabled={shouldDisableButton}/>
+
                 </form>
 
                 {getProcessingViewForStatus(this.props.status)}
